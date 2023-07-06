7a17066558df26a219d17e60c73f53273d3c2385
Daniel <calderondanieldavid@gmail.com>
2023-07-06 15:54:21 -0500

    Tercer commit - Se agrega script de analytics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,11 @@ import { Meta } from '@angular/platform-browser';
 export class HomeComponent {
   constructor(private meta:Meta) {
     meta.updateTag({property: 'og:image', content: 'https://portafoliodanieldct.netlify.app/assets/logo-portafolio.png'});
-    meta.updateTag({property: 'og:title', content: 'Desarrollador Web | Portafolio | Daniel David Calderón Tinjaca'})
-    meta.updateTag({name:'description', content:'¡Bienvenidos a mi portafolio personal! Explora una colección de mis proyectos personales y habilidades en diversos lenguajes de programación. ¡Espero que disfrutes descubriendo mi trabajo y trayectoria!'})
-    meta.updateTag({name:'copyright', content:'Copyright - Daniel David Calderón Tinjaca'})
+    meta.updateTag({property: 'og:title', content: 'Desarrollador Web | Portafolio | Daniel David Calderón Tinjaca'});
+    meta.updateTag({name:'description', content:'¡Bienvenidos a mi portafolio personal! Explora una colección de mis proyectos personales y habilidades en diversos lenguajes de programación. ¡Espero que disfrutes descubriendo mi trabajo y trayectoria!'});
+    meta.updateTag({name:'copyright', content:'Copyright - Daniel David Calderón Tinjaca'});
     meta.updateTag({name: 'author', content: 'Daniel David Calderón Tinjaca'});
-    meta.updateTag({name:'robots', content:'index, follow'})
+    meta.updateTag({name:'robots', content:'index, follow'});
     meta.updateTag({name: 'language', content: 'es'});
   }
 
